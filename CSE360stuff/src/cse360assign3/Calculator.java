0764c425583312d91beb625f6b551021c0f2e8ba
Jessie Datu <jdatu@asu.edu>
2016-02-22 11:13:20 -0700

    Initial version

--- CSE360stuff/src/cse360assign3/Calculator.java
+++ CSE360stuff/src/cse360assign3/Calculator.java
@@ -1,40 +1,68 @@
 package cse360assign3;
 
+/* * Calculator class should act like a calculator with basic functions such as add, subtract, multiply, and divide.
+ * @author Jessie Datu 
+ * PIN 226
+ * @version Feb 22, 2016.
+ * */
+
 public class Calculator 
 {
 
 	private int total;
 	
+	/** Calculator() default constructor that initializes with total = 0
+	 */
 	public Calculator () 
 	{
 		total = 0;  // not needed - included for clarity
 	}
 	
+	
+	/** getTotal should return the calculated total
+	 */
 	public int getTotal () 
 	{
 		return 0;
 	}
 	
+	/** The add method should add the parameter to the total variable
+	 * @param value being the int to be added
+	 */
 	public void add (int value) 
 	{
 		
 	}
 	
+	/** The subtract method should subtract the parameter from the total variable
+	 * @param value being the int to be subtracted
+	 */
 	public void subtract (int value) 
 	{
 		
 	}
 	
+	
+	/** The multiply method should multiply the total by the total by the parameter
+	 * @param value being the int to be multiplied
+	 */
 	public void multiply (int value) 
 	{
 		
 	}
 	
+	
+	/** The divide method should divide the total by the parameter. Use integer division. 
+	 * If the parameter is zero, set the total to zero. Do not print an error message.
+	 * @param value being the int to divide by
+	 */
 	public void divide (int value) 
 	{
 		
 	}
 	
+	/** getHistory method will return a history of all actions as a String.
+	 */
 	public String getHistory () 
 	{
 		return "";
